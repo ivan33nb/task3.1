7b6ea45b0060255d662030d1aceab830776ca40e
out-stroki-waisman <k5LpLsM5Kgjy>
2022-06-22 23:07:39 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/ivan_vaysman/task31/Application.java
@@ -0,0 +1,13 @@
+package ru.yandex.ivan_vaysman.task31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
