faf3a8cc669498a4bbd6354a61a13247feda90ad
out-stroki-waisman <k5LpLsM5Kgjy>
2022-07-27 22:59:27 +0300

    Вынес бины в конфиги

--- src/main/java/ru/yandex/ivan_vaysman/task31/config/AppConfig.java
+++ src/main/java/ru/yandex/ivan_vaysman/task31/config/AppConfig.java
@@ -1,5 +1,7 @@
 package ru.yandex.ivan_vaysman.task31.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -7,6 +9,16 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public RestTemplateBuilder restTemplateBuilder(){
+        return new RestTemplateBuilder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
 
     @Bean
     public TaskScheduler taskScheduler() {
