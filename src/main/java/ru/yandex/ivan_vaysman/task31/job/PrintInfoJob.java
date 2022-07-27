6c0516370d48e1dc85247e04d76b3dbaa678c847
out-stroki-waisman <k5LpLsM5Kgjy>
2022-07-27 23:01:17 +0300

    Заменил System.out.println, на log.info

--- src/main/java/ru/yandex/ivan_vaysman/task31/job/PrintInfoJob.java
+++ src/main/java/ru/yandex/ivan_vaysman/task31/job/PrintInfoJob.java
@@ -1,11 +1,13 @@
 package ru.yandex.ivan_vaysman.task31.job;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.yandex.ivan_vaysman.task31.service.StockInfoService;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class PrintInfoJob {
@@ -15,16 +17,16 @@ public class PrintInfoJob {
   @Async
   @Scheduled(cron = "${cron.show-info}")
   public void getTopFiveMostExpensiveTradingCompany() {
-    System.out.println(
-        "Top five most expensive trading company -> \n"
-            + stockInfoService.getTopFiveMostExpensiveTradingCompany());
+    log.info(
+        "Top five most expensive trading company -> \n{}",
+        stockInfoService.getTopFiveMostExpensiveTradingCompany());
   }
 
   @Async
   @Scheduled(cron = "${cron.show-info}")
   public void getTopFiveGreatestChangePercentInStock() {
-    System.out.println(
-        "Top five greatest change percent in stock -> \n"
-            + stockInfoService.getTopFiveGreatestChangePercentInStock());
+    log.info(
+        "Top five greatest change percent in stock -> \n{}",
+        stockInfoService.getTopFiveGreatestChangePercentInStock());
   }
 }
