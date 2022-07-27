5478818a284dd1e6af7b44408a6f3da0d2039aff
out-stroki-waisman <k5LpLsM5Kgjy>
2022-07-27 13:52:41 +0300

    Добавил TestContainer, навел порядок в gradle

--- src/test/java/ru/yandex/ivan_vaysman/task31/service/impl/StockInfoServiceImplTest.java
+++ src/test/java/ru/yandex/ivan_vaysman/task31/service/impl/StockInfoServiceImplTest.java
@@ -1,23 +1,35 @@
 package ru.yandex.ivan_vaysman.task31.service.impl;
 
-import static org.mockito.Mockito.*;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.yandex.ivan_vaysman.task31.repository.CompanyShareRepository;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class StockInfoServiceImplTest {
 
-    CompanyShareRepository companyShareRepository = mock(CompanyShareRepository.class);
+  @Container
+  public static MySQLContainer<?> mySqlDB =
+      new MySQLContainer<>("mysql:5.7.37")
+          .withInitScript("db/create-and-fill-test-table.sql");
 
-    @Test
-    void checkFindAll() {
-        companyShareRepository.findAll();
-        verify(companyShareRepository, times(1)).findAll();
-    }
+  @DynamicPropertySource
+  public static void properties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", mySqlDB::getJdbcUrl);
+    registry.add("spring.datasource.username", mySqlDB::getUsername);
+    registry.add("spring.datasource.password", mySqlDB::getPassword);
+  }
 
-    @Test
-    void getTopFiveGreatestChangePercentInStock() {
-        companyShareRepository.findAll();
-        verify(companyShareRepository, times(1)).findAll();
-    }
+  @Autowired private CompanyShareRepository companyShareRepository;
+
+  @Test
+  public void test() {
+
+  }
 }
